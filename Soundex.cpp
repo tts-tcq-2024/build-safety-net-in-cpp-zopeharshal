38c4ff67975560efe474e4bfa17e545d58171329
zopeharshal <harshalzope99@gmail.com>
2024-07-08 15:15:36 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,23 +1,25 @@
 #include "Soundex.h"
 #include <cctype>
+#include <unordered_map>
 
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
+    switch (std::toupper(c)) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0'; // A, E, I, O, U, Y, H, W map to '0'
     }
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
-    std::string soundex(1, toupper(name[0]));
+    std::string soundex;
+    soundex += std::toupper(name[0]);
+
     char prevCode = getSoundexCode(name[0]);
 
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
