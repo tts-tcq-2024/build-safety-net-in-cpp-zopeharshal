edf2bc1b0dbd0c16817b1ed32db4d8c027f2bbdc
zopeharshal <harshalzope99@gmail.com>
2024-07-18 16:07:29 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,17 @@
-#include <gtest/gtest.h>
-#include "Soundex.h"
+#include <cassert>
 
-
-TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
+void testSoundex() {
+    assert(generateSoundex("Robert") == "R163");
+    assert(generateSoundex("Rupert") == "R163");
+    assert(generateSoundex("Rubin") == "R150");
+    assert(generateSoundex("Yves") == "Y120");
+    assert(generateSoundex("A") == "A000");
+    assert(generateSoundex("") == "0000");
+    assert(generateSoundex("Carter") == "C363");
+    assert(generateSoundex("Katherine") == "K345");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
+int main() {
+    testSoundex();
+    return 0;
 }
-
