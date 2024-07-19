baaa49cc86d7a4b6202df246373a8aeb9ed8fdd8
zopeharshal <harshalzope99@gmail.com>
2024-07-19 11:14:12 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,29 +1,28 @@
+
 #include "Soundex.h"
 #include <cctype>
 #include <unordered_map>
 
 char getSoundexCode(char c) {
-    static const std::unordered_map<char, char> soundexCodes {
-        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
-        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
-        {'D', '3'}, {'T', '3'},
-        {'L', '4'},
-        {'M', '5'}, {'N', '5'},
-        {'R', '6'}
-    };
-
-    c = std::toupper(c);
-    auto it = soundexCodes.find(c);
-    if (it != soundexCodes.end()) {
-        return it->second;
+    c = toupper(c);
+   switch (std::toupper(c)) {
+        case 'B': case 'F': case 'P': case 'V': return '1';
+        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+        case 'D': case 'T': return '3';
+        case 'L': return '4';
+        case 'M': case 'N': return '5';
+        case 'R': return '6';
+        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0'; // A, E, I, O, U, Y, H, W map to '0'
     }
-    return '0'; // For A, E, I, O, U, H, W, Y and other non-matching characters
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
     std::string soundex(1, std::toupper(name[0]));
+    soundex += std::toupper(name[0]);
+
     char prevCode = getSoundexCode(name[0]);
 
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
@@ -33,10 +32,8 @@ std::string generateSoundex(const std::string& name) {
             prevCode = code;
         }
     }
-
     while (soundex.length() < 4) {
         soundex += '0';
     }
-
     return soundex;
 }
