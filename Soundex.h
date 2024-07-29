f464addb25d26f3030938b30332ced6371730d1d
zopeharshal <harshalzope99@gmail.com>
2024-07-29 16:48:32 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,8 +1,14 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
+#ifndef SOUNDEX_HPP
+#define SOUNDEX_HPP
 
 #include <string>
 
+// Declaration of functions
+char getSoundexCode(char c);
+bool SoundexLengthCheck(const std::string& soundex);
+bool SoundexCodeCheck(char code, char prevCode);
+std::string IncrementSoundex(const std::string& soundex, const std::string& name, char prevCode);
 std::string generateSoundex(const std::string& name);
+std::string padSoundex(const std::string& soundex);
 
-#endif // SOUNDEX_H
+#endif // SOUNDEX_HPP
